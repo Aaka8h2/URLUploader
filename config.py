556ef84ec6570a1f2a1af12df451594e278c4355
Aakash <kanduaakash43@gmail.com>
2024-12-20 08:08:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "6486329379"  # Replace this API ID with your actual API ID
+API_ID = "29199850"  # Replace this API ID with your actual API ID
 API_HASH = "711c12d4f9bb08a1342715862e4df9c9"  # Replace this API HASH with your actual API HASH
 BOT_TOKEN = "8063262449:AAFWJmaSgIdu2-27etCkt8X-lT6Y-OJV6o4"  # Replace this BOT_TOKEN
 SESSION_STRING = "BAG9jeoAPdTG2SfIf19LY3BiRzgXQtns0iBNoIQKplhs6eoiblweDCLEN-fxNHzULH5_vky7ytmbTw4oDzK2lpNb_KG6ry1BtS3VgKqHQ3ndsVyebu_9kd_5isB0goxTOMRnaKf2tIYR8mCDrvisn6_SEvOMHblujIg-ASYzoSSbkzvGXFB7rBbjZ0l4UAYV9LAM33NYkqsuj_1y-iLAquEs6qc0ddFKRMiOE9ieMT0zht0V5eXFtc5kjSpkzM0qKon1a_-tBGWxV4EZ39zteDAC85oAeZ9UuMIAux620xuXSX7WTtWo-jmotSSvVxFhogf48mPr1otXX8tNTHPyIDt2JSYGhAAAAAGCnYgjAA" # Replace this SESSION STRING with your actual SESSION_STRING
